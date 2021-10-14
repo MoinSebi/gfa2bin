02aace102e60c5592a5a16ce67b66b869678916a
moinSebi <s.vorbrugg@googlemail.com>
2021-10-14 14:14:12 +0200

    Fixed binary2dec_bed

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,7 @@
 pub fn binary2dec_bed(vecc: &[bool]) -> u8{
     let mut result: u8 = 0;
     let mut count = 0;
-    for x in vecc.iter().rev(){
+    for x in vecc.iter(){
         let t: u8 = 2;
         result += (t.pow(count as u32)) * (*x as u8);
         count += 1;
